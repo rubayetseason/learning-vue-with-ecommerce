c27f4db3783c8b508fc1fc70873361c3f93c1556
rubayetseason <rubayetislamseason5@gmail.com>
2025-06-02 20:27:40 +0600

    navbar sorted

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,7 +1,8 @@
 import logo from "./logos/logo.jpg";
 import logoWithoutBg from "./logos/logo-without-bg.png";
+import hero1 from "./images/hero_1.jpg";
 
 export const sharedAssets = {
   icons: { logo, logoWithoutBg },
-  images: {},
+  images: { hero1 },
 };
